75854ff784e346e73bc8b44067082c3e2fe24dd2
Rob008 <41893677+Rob008@users.noreply.github.com>
2022-03-08 18:22:17 +0400

    Todo

--- src/components/ToDoInput.jsx
+++ src/components/ToDoInput.jsx
@@ -2,42 +2,43 @@ import React,{useEffect, useState} from "react";
 import { Input ,Calendar,Button} from 'antd';
 import "../style.css"
 import Form from "antd/lib/form";
-
+import { DatePicker } from 'antd';
+import 'antd/dist/antd.css';
+// localStorage.setItem('todo',JSON.stringify([{"title":"dd","description":"ads","date_picker":"2022-02-26T16:20:28.307Z"}]));
 export const ToDoInput = () =>{
 
 
     const { TextArea } = Input;
 
-    const [toDo,setToDo]=useState([]);
-
-    const onPanelChange = (value, mode) =>{
-        // console.log(value);
-        const doDate = value._d;
-        console.log(doDate);
-      }
+    const [toDo,setToDo]=useState();
+        // console.log(localStorage.getItem("todo"));
 
       useEffect(()=>{
-        const json = localStorage.getItem("toDoing");
+        const json = localStorage.getItem("todo");
         const loadedToDo= JSON.parse(json);
         if (loadedToDo) {
             setToDo(loadedToDo);
         }
+        console.log(localStorage.getItem("todo"));
     },[]);
+    console.log(localStorage.getItem("todo"));
 
-      useEffect(()=>{
-        const json = JSON.stringify(toDo);
-        localStorage.setItem("toDoing",json);
-        console.log(localStorage)
-     },[toDo]);
+
+    //   useEffect(()=>{
+    //     const json = JSON.stringify(toDo);
+    //     localStorage.setItem("todo",json);
+    //     // console.log(localStorage)
+    //  },[toDo]);
 
       const onFinish =(values)=>{
         console.log('Success:', values);
         setToDo([...toDo].concat(values));
         console.log('Success:', toDo);
-        onPanelChange();
+        const json = JSON.stringify(toDo);
+        localStorage.setItem("todo",json);
+        // values.date =  values['date-picker'].format('YYYY-MM-DD HH:mm:ss')
+        // console.log(values);
       }
-      
-
 
     return(
         <div className="mainInput">
@@ -56,9 +57,10 @@ export const ToDoInput = () =>{
             autoComplete="off"
             //  onFinishFailed={onFinishFailed}
             >
-                <Form.Item
+                    <Form.Item
                     label="Title"
                     name="title"
+                    
                     rules={[
                     {
                     required: true,
@@ -82,21 +84,15 @@ export const ToDoInput = () =>{
                  <TextArea rows={4} placeholder="Please input Description!" maxLength={300} />
                 </Form.Item>
 
-                <Form.Item
-                    label="Calendar"
-                    name="calendar"
-                    rules={[
+                <Form.Item name="date_picker" label="Calendar" rules={[
                     {
                         required: true,
                         message: 'Please choose Date!',
                     },
-                    ]}
-                >
-                <div className="site-calendar-demo-card">
-                    <Calendar fullscreen={false} onPanelChange={onPanelChange} />
-                </div>
+                    ]}>
+                    <DatePicker showTime format="YYYY-MM-DD HH:mm:ss" />
                 </Form.Item>
-            
+                    
                 <Form.Item
                     wrapperCol={{
                     offset: 8,
--- src/components/ToDoShow.jsx
+++ src/components/ToDoShow.jsx
@@ -2,31 +2,49 @@ import React,{useEffect, useState} from "react";
 import { Input ,Calendar,Button,Card } from 'antd';
 import "../style.css"
 import Form from "antd/lib/form";
+import { SearchTodo } from "./SearchTodo";
 
 export const ToDoShow = () => {
-
+    const json = localStorage.getItem("todo");
     const [toDo,setToDo]=useState([]);
 
     useEffect(()=>{
-        const json = localStorage.getItem("toDoing");
+        const json = localStorage.getItem("todo");
         const loadedToDo= JSON.parse(json);
         if (loadedToDo) {
             setToDo(loadedToDo);
         }
+        // console.log(localStorage.getItem("todo"));
     },[]);
+    const handleSubmitEdit=(event)=>{
+        console.log(event.target.value) 
 
+    };
+    const handleSubmitDel=(item)=>{
+        localStorage.removeItem(item);
+        console.log(item)
+    }
 
 return(
 
-    <div className="toDoShow">
-        
-        <Card title="Card title" bordered={false} style={{ width: 100 }}>
-        <p>Card content</p>
-        </Card>
-        <Card title="Card title" bordered={false} style={{ width: 100 }}>
-        <p>Card content</p>
-        </Card>
+    <div className="site-card-border-less-wrapper">
+        {/* <SearchTodo/> */}
+        {console.log(toDo)}
+        {toDo.length && toDo.map((item)=>(
+                            <Card key={Math.floor(Math.random() * 9876)} title={item.title} style={{width:300,margin:20}} >
+                            <p>
+                                {item.description}
+                            </p>
+                            <p>{item.date_picker}</p>
 
+                            <div id="changeButton">
+                            <Button type="primary" id="edit" value={item.title} onClick={handleSubmitEdit}>Edit</Button>
+                            <Button id="delete" onClick={handleSubmitDel}>Delete</Button>
+                            </div>
+                        </Card>
+        )
+
+        )}
     </div>
 
 )
